74d15c63e773cb502d4a6cfa4c0e3f040ce9f332
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-21 16:38:20 +0800

    update herE

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -80,7 +80,7 @@ class _HomeScreenState extends State<HomeScreen> {
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFFDEF9C4),
                 padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                textStyle: TextStyle(fontSize: 18),
+                textStyle: const TextStyle(fontSize: 18),
               ),
             ),
             SizedBox(height: 24),
