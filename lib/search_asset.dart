ab679e80b6309ffa00fcd51ba07dc51653ed9061
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-21 12:57:54 +0800

    update herE

---
+++ lib/search_asset.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchAssetTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Search Asset Tab'),
+    );
+  }
+}
