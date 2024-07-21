defc15255c0042c5d68f8fc74ccc8986c4d29db4
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-21 13:28:39 +0800

    update herE

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _LoginScreenState extends State<LoginScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 Image.asset(
-                  'assets/logo.png', // Replace with your logo asset
+                  'assets/logo.jpg', // Replace with your logo asset
                   height: 80,
                 ),
                 const SizedBox(height: 24),
