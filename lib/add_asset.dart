f8b6716ccd6f8e5a45d3f4eab5ef13bc12a176be
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-21 12:58:14 +0800

    update herE

---
+++ lib/add_asset.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AddAssetTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Add Asset Tab'),
+    );
+  }
+}
