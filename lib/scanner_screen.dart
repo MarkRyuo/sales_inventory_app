c22f8b6038c6eb361c8e193767523c1b9cdb87c3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-22 16:27:15 +0800

    update herE

---
+++ lib/scanner_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'home_screen.dart';
+
+class ScannerScreen extends StatefulWidget {
+  const ScannerScreen({super.key});
+
+  @override
+  _ScannerScreenState createState() => _ScannerScreenState();
+}
+
+class _ScannerScreenState extends State<ScannerScreen> {
+  int _quantity = 1;
+
+  void _incrementQuantity() {
+    setState(() {
+      _quantity++;
+    });
+  }
+
+  void _decrementQuantity() {
+    setState(() {
+      if (_quantity > 1) {
+        _quantity--;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFDF7E1), // Background color
+      appBar: AppBar(
+        backgroundColor: const Color(0xFFFAF6EB),
+        elevation: 1.0,
+        shadowColor: const Color(0xFF505050).withOpacity(0.6),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(builder: (context) => const HomeScreen()),
+            );
+          },
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/p5.png', // Ensure this image exists in your assets
+              width: 200, // Adjust the width and height as needed
+              height: 200,
+            ),
+            const SizedBox(height: 24),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  icon: const Icon(Icons.add_circle, color: Colors.green),
+                  onPressed: _incrementQuantity,
+                ),
+                Text('$_quantity', style: const TextStyle(fontSize: 24)),
+                IconButton(
+                  icon: const Icon(Icons.remove_circle, color: Colors.red),
+                  onPressed: _decrementQuantity,
+                ),
+              ],
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed: () {
+                // Handle the Done button press
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF8AFF8A),
+                padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                textStyle: const TextStyle(fontSize: 18),
+              ),
+              child: const Text('Done', style: TextStyle(color: Color(0xFF3C3C3C))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
