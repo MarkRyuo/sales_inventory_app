aae3d4db767e87b87f25655a6f2e1c8974a67ab3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-28 21:23:03 +0800

    update here

--- lib/product_screen.dart
+++ lib/product_screen.dart
@@ -11,7 +11,7 @@ class ProductScreen extends StatelessWidget {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      backgroundColor: const Color(0xFFFDF7E1), // Background color
+      backgroundColor: const Color.fromARGB(255, 253, 247, 225), // Background color
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 250, 246, 235),
         elevation: 1.0, // Adds shadow
