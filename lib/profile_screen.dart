4f42738b0cd8edbaaea4ec207c9d274133f55681
MarkRyuo <malupajhonmark3035@gmail.com>
2024-07-28 21:26:31 +0800

    update here

--- lib/profile_screen.dart
+++ lib/profile_screen.dart
@@ -12,7 +12,7 @@ class ProfileScreen extends StatelessWidget {
     return Scaffold(
       backgroundColor: const Color(0xFFFDF7E1), // Background color
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 250, 246, 235),
+        backgroundColor: Color.fromARGB(255, 250, 248, 244),
         elevation: 1.0, // Adds shadow
         shadowColor: Colors.grey.withOpacity(0.6), // Customize the shadow color
         automaticallyImplyLeading: false, // Removes the back button
